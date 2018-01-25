31e7a4be1b5c0ee285ec09a7dcc1f74ee3f946c8
Phoebe Zeitler <phoebe.zeitler@gmail.com>
2018-01-25 15:23:57 -0500

    Initial framework creation

---
+++ WumpusGameRunnerConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WumpusGameRunnerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
