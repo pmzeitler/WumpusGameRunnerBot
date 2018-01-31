cf3e8265d80368dff39d38cbf6c8a3c6b36f4919
Phoebe Zeitler <phoebe.zeitler@gmail.com>
2018-01-31 17:14:46 -0500

    Created GoldenSkyStoriesModule, and added basic framework for recognizing a player/listing all players.

---
+++ GoldenSkyStoriesModule/GoldenSkyStoriesModule.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System.Threading.Tasks;
+
+namespace com.PhoebeZeitler.WumpusGameRunnerConsole.GoldenSkyStoriesModule
+{
+    public class GoldenSkyStoriesModule
+    {
+        private Dictionary<DiscordChannel, Dictionary<DiscordMember, String>> masterChannelList;
+
+
+
+
+
+        [Command("ping")] // let's define this method as a command
+        [Description("Example ping command")] // this will be displayed to tell users what this command does when they invoke help
+        [Aliases("pong")] // alternative names for the command
+        public async Task Ping(CommandContext ctx) // this command takes no arguments
+        {
+            // let's trigger a typing indicator to let
+            // users know we're working
+            await ctx.TriggerTypingAsync();
+
+            // let's make the message a bit more colourful
+            var emoji = DiscordEmoji.FromName(ctx.Client, ":ping_pong:");
+
+            // respond with ping
+            await ctx.RespondAsync($"{emoji} Pong! Ping: {ctx.Client.Ping}ms");
+        }
+
+        [Command("reg_player"), Description("Register a character name."), Aliases("regplayer")]
+        public async Task RegisterPlayer(CommandContext ctx, [Description("The character name for the player.")] string CharacterName)
+        {
+            // let's trigger a typing indicator to let
+            // users know we're working
+            await ctx.TriggerTypingAsync();
+
+            DiscordMember user = ctx.Member;
+
+            if (user == null)
+            {
+                await ctx.RespondAsync("You cannot register for a game via direct message. Please re-try your request in a channel.");
+            }
+            else
+            {
+                _RegisterPlayerInChannel(ctx.Channel, user, CharacterName);
+                await ctx.RespondAsync($"Done! {user.DisplayName} is now registered for this game as {CharacterName}.");
+            }
+        }
+
+        [Command("list_players"), Description("Register a character name."), Aliases("listall", "listplayers")]
+        public async Task ListPlayers(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+
+            DiscordChannel channel = ctx.Channel;
+
+            if (channel == null)
+            {
+                await ctx.RespondAsync("You cannot list players for a game via direct message. Please re-try your request in a channel.");
+            } else
+            {
+                Dictionary<DiscordMember, string> playerList = _GetPlayerList(channel);
+                if (playerList.Count == 0)
+                {
+                    await ctx.RespondAsync("There is no Golden Sky Stories game currently running in this channel.");
+                }
+                else
+                {
+                    foreach (var item in playerList)
+                    {
+                        await ctx.RespondAsync($"User {item.Key.Mention} is {item.Value}.");
+                    }
+                }
+            }
+        }
+
+        private Dictionary<DiscordMember, string> _GetPlayerList(DiscordChannel channel)
+        {
+            if (masterChannelList == null)
+            {
+                masterChannelList = new Dictionary<DiscordChannel, Dictionary<DiscordMember, string>>();
+            }
+            if (!masterChannelList.ContainsKey(channel))
+            {
+                masterChannelList.Add(channel, new Dictionary<DiscordMember, string>());
+            }
+
+            Dictionary<DiscordMember, string> registeredPlayers = masterChannelList[channel];
+            return registeredPlayers;
+        }
+
+        private void _RegisterPlayerInChannel(DiscordChannel channel, DiscordMember user, string characterName)
+        {
+            Dictionary<DiscordMember, string> registeredPlayers = _GetPlayerList(channel);
+
+            registeredPlayers[user] = characterName;
+        }
+
+
+
+
+
+    }
+}
--- WumpusGameRunnerConsole/WumpusGameRunnerMain.cs
+++ WumpusGameRunnerConsole/WumpusGameRunnerMain.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
+using Newtonsoft.Json;
 
 //client id: 406197905146511360
 // link to add to a server:
@@ -14,6 +16,9 @@ using DSharpPlus.EventArgs;
 
 namespace com.PhoebeZeitler.WumpusGameRunnerConsole
 {
+    public delegate void ModuleRegistration(CommandsNextModule Commands);
+
+
     // this structure will hold data from config.json
     public struct ConfigJson
     {
@@ -22,18 +27,40 @@ namespace com.PhoebeZeitler.WumpusGameRunnerConsole
 
         [JsonProperty("prefix")]
         public string CommandPrefix { get; private set; }
+
+        [JsonProperty("appIdentifier")]
+        public string AppIdentifier { get; private set; }
     }
 
     class WumpusGameRunnerConsole
     {
-        static DiscordClient discord;
+        private DiscordClient discord;
+        public CommandsNextModule Commands { get; set; }
+
+        private string appIdentifier;
+
+        //private static ModuleRegistration loadedModules = null;
 
         static void Main(string[] args)
         {
-            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+            WumpusGameRunnerConsole prog = new WumpusGameRunnerConsole();
+            prog.MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
-        static async Task MainAsync(string[] args)
+        /**
+        static void RegisterModule(ModuleRegistration module)
+        {
+            loadedModules += module;
+        }
+
+        private void LoadModules()
+        {
+            loadedModules?.Invoke(this.Commands);
+        }
+
+        **/
+
+        async Task MainAsync(string[] args)
         {
             // first, let's load our configuration file
             var json = "";
@@ -53,17 +80,99 @@ namespace com.PhoebeZeitler.WumpusGameRunnerConsole
                 LogLevel = LogLevel.Debug,
                 UseInternalLogHandler = true
             };
+
+            //set appIdentifier from config
+            this.appIdentifier = cfgjson.AppIdentifier;
             
             discord = new DiscordClient(cfg);
 
+            // up next, let's set up our commands
+            var ccfg = new CommandsNextConfiguration
+            {
+                // let's use the string prefix defined in config.json
+                StringPrefix = cfgjson.CommandPrefix,
+
+                // enable responding in direct messages
+                EnableDms = true,
+
+                // enable mentioning the bot as a command prefix
+                EnableMentionPrefix = true
+            };
+
+            // and hook them up
+            this.Commands = this.discord.UseCommandsNext(ccfg);
+
+            // let's hook some command events, so we know what's 
+            // going on
+            this.Commands.CommandExecuted += this.Commands_CommandExecuted;
+            this.Commands.CommandErrored += this.Commands_CommandErrored;
+
+            //LoadModules();
+            //This is a temporary fix while I learn & figure out more about DI and the modular system...
+            this.Commands.RegisterCommands<GoldenSkyStoriesModule.GoldenSkyStoriesModule>();
+
+            /**
             discord.MessageCreated += async e =>
             {
                 if (e.Message.Content.ToLower().StartsWith("henge"))
                     await e.Message.RespondAsync("a happy chicken!");
             };
+            **/
+
 
             await discord.ConnectAsync();
             await Task.Delay(-1);
+        }
+
+
+        private Task Client_ClientError(ClientErrorEventArgs e)
+        {
+            // let's log the details of the error that just 
+            // occured in our client
+            e.Client.DebugLogger.LogMessage(LogLevel.Error, this.appIdentifier, $"Exception occured: {e.Exception.GetType()}: {e.Exception.Message}", DateTime.Now);
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Commands_CommandExecuted(CommandExecutionEventArgs e)
+        {
+            // let's log the name of the command and user
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, this.appIdentifier, $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'", DateTime.Now);
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private async Task Commands_CommandErrored(CommandErrorEventArgs e)
+        {
+            // let's log the error details
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Error, this.appIdentifier, $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+
+            // let's check if the error is a result of lack
+            // of required permissions
+            if (e.Exception is ChecksFailedException ex)
+            {
+                // yes, the user lacks required permissions, 
+                // let them know
+
+                var emoji = DiscordEmoji.FromName(e.Context.Client, ":no_entry:");
+
+                // let's wrap the response into an embed
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Access denied",
+                    Description = $"{emoji} You do not have the permissions required to execute this command.",
+                    Color = new DiscordColor(0xFF0000) // red
+                    // there are also some pre-defined colors available
+                    // as static members of the DiscordColor struct
+                };
+                await e.Context.RespondAsync("", embed: embed);
+            }
         }
     }
 }
